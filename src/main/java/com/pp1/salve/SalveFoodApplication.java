79dce680e0a300d55276a698e862daea4fc3697f
Bernardo Gomes <bernardogomesrib@gmail.com>
2024-11-27 10:37:11 -0300

    inicio api salvefood

---
+++ src/main/java/com/pp1/salve/SalveFoodApplication.java
@@ -0,0 +1,13 @@
+package com.pp1.salve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalveFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalveFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pp1/salve/SalveFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp1.salve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalveFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
