923cc12f55372b577602d3faec5d0183247ee89f
thiagojorgelins <tjlh@discente.ifpe.edu.br>
2024-12-02 19:05:17 -0300

    feat: Add auditória do JPA (falta criar as classes de auditoria)

--- src/main/java/com/pp1/salve/SalveFoodApplication.java
+++ src/main/java/com/pp1/salve/SalveFoodApplication.java
@@ -2,8 +2,10 @@ package com.pp1.salve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SalveFoodApplication {
 
 	public static void main(String[] args) {
