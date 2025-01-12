fd9168773ac30aa7d14457021d15fcfd712f33d1
Bernardo Gomes <bernardogomesrib@gmail.com>
2025-01-12 20:04:27 -0300

    fix:funcionario não existe mais

--- src/main/java/com/pp1/salve/model/funcionario/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pp1.salve.model.funcionario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-  
-}
