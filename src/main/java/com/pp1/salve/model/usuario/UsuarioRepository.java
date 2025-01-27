8441bf1eaa2544341135df89baf52c0d1e79f4fa
thiagojorgelins <thiago.jorge2001@gmail.com>
2025-01-27 11:10:53 -0300

    feat: Adicionando validação na requisição e retornando erro caso o usuário não exista

--- src/main/java/com/pp1/salve/model/usuario/UsuarioRepository.java
+++ src/main/java/com/pp1/salve/model/usuario/UsuarioRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
     Optional<Usuario> findById(String id);
+
+    Optional<Usuario> findByEmail(String email);
 }
